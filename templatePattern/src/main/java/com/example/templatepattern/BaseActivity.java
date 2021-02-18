bf9e92d8d2e6239b5b1cf6e5584893ba6b04e5df
Cai Huijian <hjcai@telenav.cn>
2021-02-18 16:58:57 +0800

    ioc module 重命名为 baselibrary 模板模式学习

---
+++ templatePattern/src/main/java/com/example/templatepattern/BaseActivity.java
@@ -0,0 +1,60 @@
+package com.example.templatepattern;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by hjcai on 2021/2/8.
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // 设置布局layout
+        setContentView();
+
+        // 初始化头部
+        initTitle();
+
+        // 初始化界面
+        initView();
+
+        // 初始化数据
+        initData();
+    }
+
+    // 初始化数据
+    protected abstract void initData();
+
+    // 初始化界面
+    protected abstract void initView();
+
+    // 初始化头部
+    protected abstract void initTitle();
+
+    // 设置布局layout
+    protected abstract void setContentView();
+
+
+    /**
+     * 启动Activity
+     */
+    protected void startActivity(Class<?> clazz){
+        Intent intent = new Intent(this,clazz);
+        startActivity(intent);
+    }
+
+    /**
+     * findViewById
+     * @return View
+     */
+    protected <T extends View> T viewById(int viewId){
+        return (T) findViewById(viewId);
+    }
+}
